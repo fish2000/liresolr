e29a449f99e79cfb1fe2224e912ed12de969082a
dermotte <mathias@juggle.at>
2013-07-07 18:34:55 +0200

    xml based update

--- src/main/java/net/semanticmetadata/lire/solr/AddImages.java
+++ src/main/java/net/semanticmetadata/lire/solr/AddImages.java
@@ -1,7 +1,6 @@
 package net.semanticmetadata.lire.solr;
 
-import net.semanticmetadata.lire.imageanalysis.ColorLayout;
-import net.semanticmetadata.lire.imageanalysis.EdgeHistogram;
+import net.semanticmetadata.lire.imageanalysis.*;
 import net.semanticmetadata.lire.indexing.hashing.BitSampling;
 import net.semanticmetadata.lire.utils.FileUtils;
 import net.semanticmetadata.lire.utils.SerializationUtils;
@@ -13,6 +12,7 @@ import java.io.*;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 
 /**
@@ -42,30 +42,47 @@ import java.util.Iterator;
 // <delete><query>id:*</query></delete>
 
 public class AddImages {
+    static String baseURL = "http://localhost:9000/solr/lire";
+
     public static void main(String[] args) throws IOException {
         BitSampling.readHashFunctions();
         ArrayList<File> files = FileUtils.getAllImageFiles(new File("D:\\DataSets\\WIPO\\CA\\converted-4"), true);
+        /*
 //        ArrayList<File> files = FileUtils.getAllImageFiles(new File("..\\Lire\\testdata\\wang-1000"), true);
         for (Iterator<File> iterator = files.iterator(); iterator.hasNext(); ) {
             File file = iterator.next();
-            URL u = new URL("http://localhost:8080/solr/lire/update?stream.body=" +
+            // System.out.println(createAddDoc(file).toString());
+            URL u = new URL(baseURL + "/update?stream.body=" +
                     URLEncoder.encode(createAddDoc(file).toString(), "utf-8"));
             InputStream in = u.openStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(in));
             String line;
-            while ((line = reader.readLine())!=null) {
+            while ((line = reader.readLine()) != null) {
                 System.out.println(line);
             }
             System.out.println("******");
         }
-        System.out.println("http://localhost:8080/solr/lire/update?stream.body=" + URLEncoder.encode("<commit/>", "utf-8"));
+        */
+        int count = 0;
+        BufferedWriter br = new BufferedWriter(new FileWriter("add.xml", false));
+        br.write("<add>\n");
+        for (Iterator<File> iterator = files.iterator(); iterator.hasNext(); ) {
+            File file = iterator.next();
+            br.write(createAddDoc(file).toString());
+            count++;
+            if (count % 10 == 0) System.out.print('.');
+            if (count % (40*10) == 0) System.out.println(" " + count);
+        }
+        br.write("</add>\n");
+        br.close();
+        System.out.println(baseURL + "/update?stream.body=" + URLEncoder.encode("<commit/>", "utf-8"));
 //        System.out.println("http://localhost:8080/solr/lire/update?stream.body=" + URLEncoder.encode("<commit/>", "utf-8"));
     }
 
     static StringBuilder createAddDoc(File image) throws IOException {
         BufferedImage img = ImageIO.read(image);
         StringBuilder result = new StringBuilder(200);
-        result.append("<add>\n");
+//        result.append("<add>\n");
         result.append("\t<doc>\n");
         // id and file name ...
         result.append("\t\t<field name=\"id\">");
@@ -74,20 +91,25 @@ public class AddImages {
         result.append("\t\t<field name=\"title\">");
         result.append(image.getName());
         result.append("</field>\n");
-        // feature:
-        ColorLayout eh = new ColorLayout ();
-        eh.extract(img);
-        result.append("\t\t<field name=\"histogram\">");
-        result.append(Base64.encodeBase64String(eh.getByteArrayRepresentation()));
-        result.append("</field>\n");
-        // hashes
-        result.append("\t\t<field name=\"hashes\">");
-        result.append(SerializationUtils.arrayToString(BitSampling.generateHashes(eh.getDoubleHistogram())));
-        result.append("</field>\n");
+        // features:
+        getFields(img, result, new ColorLayout(), "cl_hi", "cl_ha");
+        getFields(img, result, new EdgeHistogram(), "eh_hi", "eh_ha");
+        getFields(img, result, new JCD(), "jc_hi", "jc_ha");
+        getFields(img, result, new PHOG(), "ph_hi", "ph_ha");
+        // close doc ...
         result.append("\t</doc>\n");
-        result.append("</add>");
-
-
+//        result.append("</add>");
         return result;
     }
+
+    private static void getFields(BufferedImage img, StringBuilder result, LireFeature feature, String histogramField, String hashesField) {
+        feature.extract(img);
+        result.append("\t\t<field name=\"" + histogramField + "\">");
+        result.append(Base64.encodeBase64String(feature.getByteArrayRepresentation()));
+        result.append("</field>\n");
+        result.append("\t\t<field name=\"" + hashesField + "\">");
+        result.append(SerializationUtils.arrayToString(BitSampling.generateHashes(feature.getDoubleHistogram())));
+        result.append("</field>\n");
+    }
+
 }
