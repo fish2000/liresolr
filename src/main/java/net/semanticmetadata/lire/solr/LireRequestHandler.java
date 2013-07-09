6d6e80ec91f88816b318e5b7722c4ca5613a73bc
dermotte <mathias@juggle.at>
2013-07-09 12:29:35 +0200

    search handler prototype finished

--- src/main/java/net/semanticmetadata/lire/solr/LireRequestHandler.java
+++ src/main/java/net/semanticmetadata/lire/solr/LireRequestHandler.java
@@ -1,10 +1,28 @@
 package net.semanticmetadata.lire.solr;
 
+import net.semanticmetadata.lire.DocumentBuilder;
+import net.semanticmetadata.lire.imageanalysis.*;
+import net.semanticmetadata.lire.impl.GenericDocumentBuilder;
+import net.semanticmetadata.lire.impl.SimpleResult;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.BooleanClause;
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.TermQuery;
+import org.apache.solr.client.solrj.response.TermsResponse;
 import org.apache.solr.common.params.SolrParams;
 import org.apache.solr.common.util.NamedList;
 import org.apache.solr.handler.RequestHandlerBase;
 import org.apache.solr.request.SolrQueryRequest;
 import org.apache.solr.response.SolrQueryResponse;
+import org.apache.solr.search.DocIterator;
+import org.apache.solr.search.DocSet;
+import org.apache.solr.search.SolrIndexSearcher;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.TreeSet;
 
 /**
  * This file is part of LIRE, a Java library for content based image retrieval.
@@ -14,6 +32,15 @@ import org.apache.solr.response.SolrQueryResponse;
 
 public class LireRequestHandler extends RequestHandlerBase {
     private int countRequests = 0;
+    private static HashMap<String, Class> fieldToClass = new HashMap<String, Class>(5);
+
+    static {
+        fieldToClass.put("cl_ha", ColorLayout.class);
+        fieldToClass.put("ph_ha", PHOG.class);
+        fieldToClass.put("oh_ha", OpponentHistogram.class);
+        fieldToClass.put("eh_ha", EdgeHistogram.class);
+        fieldToClass.put("jc_ha", JCD.class);
+    }
 
     @Override
     public void init(NamedList args) {
@@ -23,8 +50,76 @@ public class LireRequestHandler extends RequestHandlerBase {
     @Override
     public void handleRequestBody(SolrQueryRequest req, SolrQueryResponse rsp) throws Exception {
         SolrParams params = req.getParams();
-        if (params.get("request").equals("add")) countRequests++;
-        rsp.add("Test-name", "Test-val");
+        SolrIndexSearcher searcher = req.getSearcher();
+        // get the params needed:
+        // hashes=x y z ...
+        // feature=<base64>
+        // field=<cl_ha|ph_ha|...>
+        String[] hashes = params.get("hashes").trim().split(" ");
+        byte[] featureVector = Base64.decodeBase64(params.get("feature"));
+        String field = params.get("field");
+        int maximumHits = 20;
+        if (params.getInt("rows")!=null)
+            maximumHits = params.getInt("rows");
+        // create boolean query:
+//        System.out.println("** Creating query.");
+        BooleanQuery query = new BooleanQuery();
+        for (int i = 0; i < hashes.length; i++) {
+            // be aware that the hashFunctionsFileName of the field must match the one you put the hashes in before.
+            hashes[i] = hashes[i].trim();
+            if (hashes[i].length() > 0)
+                query.add(new BooleanClause(new TermQuery(new Term(field, hashes[i] + "")), BooleanClause.Occur.SHOULD));
+        }
+//        System.out.println("** Doing search.");
+        // get results:
+        DocSet docSet = searcher.getDocSet(query);
+        DocIterator iterator = docSet.iterator();
+        // temp feature
+        LireFeature f = (LireFeature) fieldToClass.get(field).newInstance();
+        // query feature
+        LireFeature q = (LireFeature) fieldToClass.get(field).newInstance();
+        q.setByteArrayRepresentation(featureVector);
+//        System.out.println("** Doing re-rank.");
+        // re-rank
+        TreeSet<SimpleResult> resultScoreDocs = new TreeSet<SimpleResult>();
+        float maxDistance = -1f;
+        float tmpScore;
+
+        int count = 0;
+        String name = field.replace("_ha", "_hi");
+//        System.out.println("** Iterating docs.");
+        while (count++ < 500 && iterator.hasNext()) {
+//            System.out.println("** " + count);
+            int i = iterator.nextDoc();
+//            System.out.println("** Getting document " + i);
+            Document d = searcher.doc(i);
+//            System.out.println("** Getting data from field " + name);
+            f.setByteArrayRepresentation(d.getBinaryValue(name).bytes, d.getBinaryValue(name).offset, d.getBinaryValues(name).length);
+            tmpScore = q.getDistance(f);
+            assert (tmpScore >= 0);
+            if (resultScoreDocs.size() < maximumHits) {
+                resultScoreDocs.add(new SimpleResult(tmpScore, d, count));
+                maxDistance = Math.max(maxDistance, tmpScore);
+            } else if (tmpScore < maxDistance) {
+                // if it is nearer to the sample than at least one of the current set:
+                // remove the last one ...
+                resultScoreDocs.remove(resultScoreDocs.last());
+                // add the new one ...
+                resultScoreDocs.add(new SimpleResult(tmpScore, d, count));
+                // and set our new distance border ...
+                maxDistance = resultScoreDocs.last().getDistance();
+            }
+        }
+//        System.out.println("** Creating response.");
+        // todo: create response:
+        for (Iterator<SimpleResult> it = resultScoreDocs.iterator(); it.hasNext(); ) {
+            SimpleResult result = it.next();
+            HashMap m = new HashMap(2);
+            m.put("d", result.getDistance());
+            m.put("id", result.getDocument().get("title"));
+            rsp.add("r", m);
+        }
+        // rsp.add("Test-name", "Test-val");
     }
 
     @Override
--- src/main/java/net/semanticmetadata/lire/solr/SearchImages.java
+++ src/main/java/net/semanticmetadata/lire/solr/SearchImages.java
@@ -1,5 +1,8 @@
 package net.semanticmetadata.lire.solr;
 
+import net.semanticmetadata.lire.imageanalysis.ColorLayout;
+import net.semanticmetadata.lire.imageanalysis.LireFeature;
+import org.apache.commons.codec.binary.Base64;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
@@ -11,6 +14,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 
 /**
  * This file is part of LIRE, a Java library for content based image retrieval.
@@ -19,6 +23,7 @@ import java.net.URLEncoder;
  */
 
 public class SearchImages {
+    private static String baseURL = "http://gtxzilla.itec.uni-klu.ac.at:9000/solr/lire/get";
     public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
         // http://localhost:8080/solr/lire/query?q=hashes%3A1152++hashes%3A605++hashes%3A96++hashes%3A275++&wt=xml
         String hashes = "1152  605  96  275  2057  3579  3950  2831  2367  3169  3292  974  2465  1573  2933  3125  314  2158  3532  974  2198  2315  3013  3302  3316  1467  2213  818  3  1083  18  2604  327  1370  593  3677  464  79  256  984  2496  1124  855  2091  780  1941  1887  1145  1396  4016  2406  2227  1532  2598  215  1375  171  2516  1698  368  2350  3799  223  1471  2083  1051  3015  3789  3374  1442  3991  3575  1452  751  428  3103  1182  2241  474  275  3678  3970  559  3394  2662  2361  2048  1083  181  1483  3903  3331  2363  756  558  2838  3984  1878  2667  3333  1473  2136  3499  3873  1437  3091  1287  948  46  3660  3003  1572  1185  2231  2622  257  3538  3632  3989  1180  3928  3144  1492  3941  3253  3498  2721  1036  22  1020  725  1431  3821  2248  2542  3659  2849  524  2967  1  2493  3620  2951  3584  1641  3873  2087  1506  1489  3064";
@@ -26,16 +31,29 @@ public class SearchImages {
         String query = "";
         for (int i = 0; i < split.length; i++) {
             String s = split[i];
-            if (s.trim().length() > 0) query += " hashes:" + s.trim();
+            if (s.trim().length() > 0) query += " cl_ha:" + s.trim();
         }
-        URL u = new URL("http://localhost:8080/solr/lire/query?q=" + URLEncoder.encode(query, "utf-8") + "&wt=xml&rows=500");
+        URL u = new URL(baseURL + "?q=" + URLEncoder.encode(query, "utf-8") + "&wt=xml&rows=500");
         InputStream in = u.openStream();
         SAXParser saxParser = SAXParserFactory.newInstance().newSAXParser();
-        saxParser.parse(in, new SolrResponseHandler());
-        System.out.println("******");
+        SolrResponseHandler dh = new SolrResponseHandler();
+        saxParser.parse(in, dh);
+        ArrayList<ResultItem> results = dh.getResults();
+        // re-rank:
+
     }
 
 
+}
+
+class ResultItem {
+    private LireFeature feature;
+    private String id;
+
+    ResultItem(LireFeature feature, String id) {
+        this.feature = feature;
+        this.id = id;
+    }
 }
 
 class SolrResponseHandler extends DefaultHandler {
@@ -45,13 +63,14 @@ class SolrResponseHandler extends DefaultHandler {
     int countResults = 0;
     StringBuilder hist = new StringBuilder(256);
     StringBuilder id = new StringBuilder(256);
+    private ArrayList<ResultItem> results = new ArrayList<ResultItem>(500);
 
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
         if (qName.startsWith("doc")) {
             isInDocument = true;
         } else if (isInDocument && qName.startsWith("str")) {
-            if (attributes.getValue("name") != null && attributes.getValue("name").equals("histogram")) {
+            if (attributes.getValue("name") != null && attributes.getValue("name").equals("cl_hi")) {
                 isInHistogram = true;
                 hist.delete(0, hist.length());
             } else if (attributes.getValue("name") != null && attributes.getValue("name").equals("id")) {
@@ -68,6 +87,9 @@ class SolrResponseHandler extends DefaultHandler {
             isInDocument = false;
             countResults++;
             System.out.println(id.toString() + ", " + hist.toString());
+            ColorLayout cl = new ColorLayout();
+            cl.setByteArrayRepresentation(Base64.decodeBase64(hist.toString()));
+            results.add(new ResultItem(cl, id.toString()));
         } else if (qName.startsWith("str")) {
             isInHistogram = false;
             isInId = false;
@@ -80,5 +102,9 @@ class SolrResponseHandler extends DefaultHandler {
     public void characters(char[] ch, int start, int length) throws SAXException {
         if (isInHistogram) hist.append(ch, start, length);
         if (isInId) id.append(ch, start, length);
+    }
+
+    ArrayList<ResultItem> getResults() {
+        return results;
     }
 }
