fb02f55a9b00e93d9795bd67c8688d56376fc966
dermotte <mathias@juggle.atl>
2013-12-18 11:22:52 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/main/config/build.properties

---
+++ src/main/java/net/semanticmetadata/lire/solr/LireEntityProcessor.java
@@ -0,0 +1,77 @@
+package net.semanticmetadata.lire.solr;
+
+import net.semanticmetadata.lire.imageanalysis.*;
+import net.semanticmetadata.lire.indexing.hashing.BitSampling;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.solr.handler.dataimport.Context;
+import org.apache.solr.handler.dataimport.DataSource;
+import org.apache.solr.handler.dataimport.EntityProcessorBase;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * An entity processor like the one for Tika to support data base imports and alike
+ *
+ * @author Mathias Lux, mathias@juggle.at on 17.12.13.
+ */
+public class LireEntityProcessor extends EntityProcessorBase {
+    protected boolean done = false;
+    protected LireFeature[] listOfFeatures = new LireFeature[]{
+            new ColorLayout(), new PHOG(), new EdgeHistogram(), new JCD(), new OpponentHistogram()
+    };
+    protected static HashMap<Class, String> classToPrefix = new HashMap<Class, String>(5);
+    int count = 0;
+
+    static {
+        classToPrefix.put(ColorLayout.class, "cl");
+        classToPrefix.put(EdgeHistogram.class, "eh");
+        classToPrefix.put(PHOG.class, "ph");
+        classToPrefix.put(OpponentHistogram.class, "oh");
+        classToPrefix.put(JCD.class, "jc");
+    }
+
+
+    protected void firstInit(Context context) {
+        super.firstInit(context);
+        done = false;
+    }
+
+    /**
+     * @return a row where the key is the name of the field and value can be any Object or a Collection of objects. Return
+     * null to signal end of rows
+     */
+    public Map<String, Object> nextRow() {
+        if (done) return null;
+        Map<String, Object> row = new HashMap<String, Object>();
+        DataSource<InputStream> dataSource = context.getDataSource();
+        System.out.println("\n**** " + context.getResolvedEntityAttribute("filepath"));
+
+        InputStream is = dataSource.getData(context.getResolvedEntityAttribute("filepath"));
+        // here we have to open the stream and extract the features. Then we put them into the row object.
+        // basically I hope that the entity processor is called for each entity anew, otherwise this approach won't work.
+        try {
+            BufferedImage img = ImageIO.read(is);
+            row.put("id", context.getResolvedEntityAttribute("filepath"));
+            for (int i = 0; i < listOfFeatures.length; i++) {
+                LireFeature feature = listOfFeatures[i];
+                feature.extract(img);
+                String histogramField = classToPrefix.get(feature.getClass()) + "_hi";
+                String hashesField = classToPrefix.get(feature.getClass()) + "_ha";
+                row.put(histogramField, Base64.encodeBase64String(feature.getByteArrayRepresentation()));
+                row.put(hashesField, ParallelSolrIndexer.arrayToString(BitSampling.generateHashes(feature.getDoubleHistogram())));
+            }
+        } catch (IOException e) {
+            System.err.println("Error loading image or extracting features.");
+            e.printStackTrace();
+        }
+        // if (count++>10)
+            done = true;
+        dataSource.close();
+        return row;
+    }
+}
