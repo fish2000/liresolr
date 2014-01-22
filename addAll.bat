2312bbc70ab600c27429164fde0df04a6a8b24bb
Alexander Bohn / FI$H2k <fish2000@gmail.com>
2014-01-22 08:56:02 -0500

    Refashioned “addAll.bat” into a proper script

--- addAll.bat
+++
@@ -1,5 +0,0 @@
-curl http://localhost:9000/solr/lire/update  -H "Content-Type: text/xml" --data-binary @add0.xml
-curl http://localhost:9000/solr/lire/update  -H "Content-Type: text/xml" --data-binary @add1.xml
-curl http://localhost:9000/solr/lire/update  -H "Content-Type: text/xml" --data-binary @add2.xml
-curl http://localhost:9000/solr/lire/update  -H "Content-Type: text/xml" --data-binary @add3.xml
-curl http://localhost:9000/solr/lire/update  -H "Content-Type: text/xml" --data-binary "<commit/>"
